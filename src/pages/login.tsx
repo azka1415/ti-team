1de339b96d1a803cac4620e9f124e00edba27d52
azka1415 <rafifzaydan4@gmail.com>
2022-12-11 01:24:41 +0700

    created login page

---
+++ src/pages/login.tsx
@@ -0,0 +1,11 @@
+import { Box } from '@chakra-ui/react'
+import React from 'react'
+import Navbar from '../components/navbar/Navbar'
+
+export default function login() {
+    return (
+        <Box>
+            <Navbar />
+        </Box>
+    )
+}
