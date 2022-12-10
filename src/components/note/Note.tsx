2f6e0e4adabd8f76b1aae0f5a9147ce7521bd200
azka1415 <rafifzaydan4@gmail.com>
2022-12-11 01:24:17 +0700

    edit note component

--- src/components/note/Note.tsx
+++ src/components/note/Note.tsx
@@ -1,4 +1,4 @@
-import { Box, Card, CardBody, CardHeader, Flex, Heading, Text, Divider, CardFooter } from '@chakra-ui/react'
+import { Box, Card, CardBody, CardHeader, Flex, Heading, Text, Divider, CardFooter, Checkbox, Button } from '@chakra-ui/react'
 import React from 'react'
 
 export default function Note() {
@@ -6,30 +6,48 @@ export default function Note() {
         <Card variant={'elevated'}
             textColor={'white'}
             shadow={'2xl'}
+            w={'auto'}
+            h={'auto'}
+            maxH={'sm'}
+            maxW={'sm'}
         >
             <CardHeader bg={'yellow.500'} textColor={'black'}>
-                <Box as='div' className='min-w-[21rem] max-w-[21rem]'>
+                <Box className='w-auto'>
 
-                    <Flex justify={'start'}>
+                    <Flex justify={'start'} gap='2'>
 
                         <Heading size={'md'} >
                             Note Title
                         </Heading>
+                        <Checkbox
+                            colorScheme={'green'}
+                            isDisabled defaultChecked={true}
+                        />
                     </Flex>
                 </Box>
             </CardHeader>
             <CardBody bg={'yellow.300'} >
-                <Box as='div' rounded={'lg'} textColor={'brown'} className='flex flex-col justify-center items-center'>
+                <Box rounded={'lg'} textColor={'brown'} p='2'>
+                    <Flex direction={'column'} justify='center'
+                        alignItems={'center'} h={'full'}>
 
 
-                    <Text fontWeight={'bold'} >
-                        body test
-                    </Text>
+                        <Text fontWeight={'bold'} >
+                            body test
+                        </Text>
+                    </Flex>
                 </Box>
             </CardBody>
+            <Box h={0.5} bg='yellow.500'></Box>
             <CardFooter bg={'yellow.300'} textColor={'black'}>
-                {new Date().toLocaleDateString()}
+                <Box display="flex" alignItems="center" justifyContent="space-between" w={'full'}>
+                    <Text>
+                        {new Date().toLocaleString()}
+                    </Text>
+                    <Button size={'sm'} colorScheme='green' >Check</Button>
+                    <Button size={'sm'} colorScheme='red'>Delete</Button>
+                </Box>
             </CardFooter>
-        </Card>
+        </Card >
     )
 }
