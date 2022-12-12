8a5d2d868c072be43654c53cc8904b4229dc259e
azka1415 <rafifzaydan4@gmail.com>
2022-12-12 15:03:16 +0700

    created login page

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,8 +1,47 @@
-import React from 'react'
+import { signIn, signOut, useSession } from 'next-auth/react';
+import React, { useState } from 'react'
 import Navbar from '../components/navbar/Navbar'
 
-export default function login() {
+export default function Login() {
+    const { data: session } = useSession();
+    const [loading, setLoading] = useState(false);
+    const handleSignOut = async () => {
+        setLoading(true);
+        await signOut();
+        setLoading(false);
+    }
+    const handleSignIn = async () => {
+        setLoading(true);
+        await signIn('google');
+        setLoading(false);
+    }
+
     return (
-        <Navbar />
+        <>
+            <Navbar />
+            <div className='flex justify-center h-screen items-center w-screen scrollbar-hide'>
+                <div className='flex flex-col space-y-2 p-2'>
+                    {session && (
+                        <>
+                            <h1 className='text-2xl font-bold'>
+                                Welcome! {session.user?.name}
+                            </h1>
+                            <p>
+                                ({session.user?.email})
+                            </p>
+                            <button className='bg-blue-500 p-2 rounded-md text-white' onClick={handleSignOut}>
+                                {loading ? 'Logging out...' : 'Log out'}</button>
+                        </>
+                    )}
+                    {!session && (
+                        <>
+                            <h1 className='text-4xl font-bold'>Login</h1>
+                            <button className='bg-blue-500 p-2 rounded-md text-white' onClick={handleSignIn}>{loading ? 'Logging in...' : 'login'}</button>
+                        </>
+                    )}
+                </div>
+
+            </div>
+        </>
     )
 }
