651520bbadb7edc2aa8d19bb1e1945863817125e
azka1415 <rafifzaydan4@gmail.com>
2022-12-13 16:15:56 +0700

    created note component

--- src/components/note/Note.tsx
+++ src/components/note/Note.tsx
@@ -1,7 +1,27 @@
-
 export default function Note() {
     return (
-        <div>
+        <div className="flex flex-col w-full p-2 shadow-2xl rounded-lg bg-gray-100">
+            <div className="flex flex-col space-y-2 p-2 text-start  rounded-lg">
+
+
+                <div className="w-min">
+                    <h2 className="text-xl font-semibold">super duper long title like really long wkwkwkwwk</h2>
+                </div>
+                <hr />
+                <p>body</p>
+                <div className="flex justify-between items-center">
+
+                    <p>
+                        {new Date().toLocaleDateString()}
+                    </p>
+                    <div className="flex justify-end items-center space-x-4">
+                        <button className="bg-green-500 p-2 rounded-lg">Check</button>
+                        <button className="bg-red-500 p-2 rounded-lg">Delete</button>
+                    </div>
+                </div>
+                <div>
+                </div>
+            </div>
         </div>
     )
 }
