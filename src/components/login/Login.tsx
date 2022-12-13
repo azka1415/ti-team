6ee0dc7fe48db55ad62efda9566a8f284df295be
azka1415 <rafifzaydan4@gmail.com>
2022-12-13 16:15:21 +0700

    login component

---
+++ src/components/login/Login.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from '../navbar/Navbar'
+
+export default function Login() {
+    return (
+        <>
+            <Navbar />
+            <div className='flex justify-center items-center h-screen'>
+                Please Login
+            </div>
+        </>
+    )
+}
