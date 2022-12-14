d43169e2e7ae2c6036212e8cd1f235ac8559a901
azka1415 <rafifzaydan4@gmail.com>
2022-12-14 12:40:00 +0700

    overflow handler

--- src/components/note/Note.tsx
+++ src/components/note/Note.tsx
@@ -42,23 +42,24 @@ export default function Note({ item, refetch }: Props) {
             as={Fragment}
             show={deleted}
             leave="transform duration-200 transition ease-in-out"
-            leaveFrom="opacity-100 rotate-0 scale-100 "
-            leaveTo="opacity-0 scale-95 "
+            leaveFrom="opacity-100 rotate-0 scale-100"
+            leaveTo="opacity-0 scale-95 transform -translate-x-[4rem]"
 
         >
 
             <div className="flex flex-col w-full p-2 shadow-2xl rounded-lg bg-gray-100">
-                <div className="flex flex-col space-y-2 p-2 text-start  rounded-lg">
+                <div className="flex flex-col space-y-2 p-2 text-start rounded-lg h-80 ">
+                    <p className={`text-xl font-semibold break-all ${item.checked ? 'line-through' : ''}`}>
+                        {item.name}
+                    </p>
                     <div className="w-min flex space-x-2 items-center">
-                        <h2 className={`text-xl font-semibold ${item.checked ? 'line-through' : ''}`}>
-                            {item.name}
-                        </h2>
                         {handleCheck.isLoading && <div>Updating...</div>}
-                        {handleDelete.isLoading && <div>Deleting...</div>}
                     </div>
                     <hr />
-                    <p>{item.body}</p>
-                    <div className="flex justify-between items-center">
+                    <div className="overflow-auto h-full ">
+                        <p className="break-all">{item.body}</p>
+                    </div>
+                    <div className="flex justify-center items-end h-full space-x-2 my-auto">
                         <p>
                             {new Date(item.createdAt).toLocaleString()}
                         </p>
@@ -70,6 +71,6 @@ export default function Note({ item, refetch }: Props) {
 
                 </div>
             </div>
-        </Transition>
+        </Transition >
     )
 }
