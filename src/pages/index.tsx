72a645890e1f892dc97c59862be063f4052a8ddd
azka1415 <rafifzaydan4@gmail.com>
2022-12-21 13:38:47 +0700

    render errors

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -84,71 +84,74 @@ const Home: NextPage = () => {
             </button>
           </div>
         </div>
-        <div className="flex w-full flex-col items-center justify-center space-y-1 px-2 md:flex-row md:justify-start md:space-x-2 md:space-y-0">
-          <div className="flex h-fit w-fit items-center justify-center rounded-lg bg-gray-200 pr-2">
-            <input
-              type="text"
-              value={query}
-              onChange={(e) => setQuery(e.target.value)}
-              id="search"
-              className="rounded-lg bg-gray-200 p-2 text-sm outline-none transition-all"
-              autoComplete="off"
-              placeholder="Search Notes"
-            />
+        {notes.length > 1 && (
+          <div className="flex w-full flex-col items-center justify-center space-y-1 px-2 md:flex-row md:justify-start md:space-x-2 md:space-y-0">
+            <div className="flex h-fit w-fit items-center justify-center rounded-lg bg-gray-200 pr-2">
+              <input
+                type="text"
+                value={query}
+                onChange={(e) => setQuery(e.target.value)}
+                id="search"
+                className="rounded-lg bg-gray-200 p-2 text-sm outline-none transition-all"
+                autoComplete="off"
+                placeholder="Search Notes"
+              />
 
-            <SearchIcon
-              className={`flex items-center justify-center ${
-                query.length === 0 ? "visible" : "collapse"
-              }`}
-            />
+              <SearchIcon
+                className={`flex items-center justify-center ${
+                  query.length === 0 ? "visible" : "collapse"
+                }`}
+              />
 
-            <button
-              className={`flex items-center justify-center ${
-                query.length !== 0 ? "visible" : "hidden"
-              }`}
-              onClick={() => setQuery("")}
-            >
-              <CloseIcon className="flex w-2" />
-            </button>
-          </div>
-          <div className="flex gap-2 rounded-lg border p-2">
-            <label htmlFor="check" className="text-sm">
-              Show Completed Notes
-            </label>
-            <input
-              type="checkbox"
-              checked={completed}
-              onChange={(e) => handleNoteFilter(e)}
-              id="checked_only"
-              className="rounded-lg bg-gray-200 p-2 text-sm outline-none transition-all"
-            />
-          </div>
-          <div className="flex space-x-2 rounded-lg border p-2">
-            <label htmlFor="check" className="text-sm">
-              Show Uncompleted Notes
-            </label>
-            <input
-              type="checkbox"
-              checked={uncompleted}
-              onChange={(e) => handleNoteFilter(e)}
-              id="unchecked_only"
-              className="rounded-lg bg-gray-200 p-2 text-sm outline-none transition-all"
-            />
-          </div>
-          <Transition
-            show={!found}
-            enter="transition ease-out duration-300"
-            enterFrom="transform translate-y-4"
-            enterTo="transform translate-y-0"
-            leave="transition ease-in duration-200"
-            leaveFrom="transform translate-y-0"
-            leaveTo="transform translate-y-4"
-          >
-            <div className="flex items-center justify-center p-2">
-              {!found && <p>No Notes Found</p>}
+              <button
+                className={`flex items-center justify-center ${
+                  query.length !== 0 ? "visible" : "hidden"
+                }`}
+                onClick={() => setQuery("")}
+              >
+                <CloseIcon className="flex w-2" />
+              </button>
             </div>
-          </Transition>
-        </div>
+            <div className="flex gap-2 rounded-lg border p-2">
+              <label htmlFor="check" className="text-sm">
+                Show Completed Notes
+              </label>
+              <input
+                type="checkbox"
+                checked={completed}
+                onChange={(e) => handleNoteFilter(e)}
+                id="checked_only"
+                className="rounded-lg bg-gray-200 p-2 text-sm outline-none transition-all"
+              />
+            </div>
+            <div className="flex space-x-2 rounded-lg border p-2">
+              <label htmlFor="check" className="text-sm">
+                Show Uncompleted Notes
+              </label>
+              <input
+                type="checkbox"
+                checked={uncompleted}
+                onChange={(e) => handleNoteFilter(e)}
+                id="unchecked_only"
+                className="rounded-lg bg-gray-200 p-2 text-sm outline-none transition-all"
+              />
+            </div>
+            <Transition
+              show={!found}
+              appear={true}
+              enter="transition ease-out duration-300"
+              enterFrom="transform translate-y-4"
+              enterTo="transform translate-y-0"
+              leave="transition ease-in duration-200"
+              leaveFrom="transform translate-y-0"
+              leaveTo="transform translate-y-4"
+            >
+              <div className="flex items-center justify-center p-2">
+                {!found && <p>No Notes Found</p>}
+              </div>
+            </Transition>
+          </div>
+        )}
         <AddNote
           openModal={openModal}
           refetch={items.refetch}
@@ -169,25 +172,45 @@ const Home: NextPage = () => {
           leaveTo="transform opacity-0 scale-95"
           className="flex flex-col items-center justify-center space-y-4 p-2 transition-all md:grid md:grid-cols-3 md:gap-4 md:space-y-0 lg:grid-cols-5"
         >
-          {filteredNotes.map((item) => (
-            <Transition.Child
-              key={item.id}
-              appear={true}
-              enter="transition ease-out duration-300"
-              enterFrom="transform scale-100 -translate-x-8"
-              enterTo="transform scale-100 -translate-x-0"
-              leave="transform duration-200 transition ease-in-out"
-              leaveFrom="opacity-100 rotate-0 scale-100"
-              leaveTo="opacity-0 scale-75 transform -translate-x-[4rem]"
-            >
-              <Note
-                item={item}
-                refetch={items.refetch}
-                setNotes={setNotes}
-                notes={notes}
-              />
-            </Transition.Child>
-          ))}
+          {notes.length > 1
+            ? filteredNotes.map((item) => (
+                <Transition.Child
+                  key={item.id}
+                  appear={true}
+                  enter="transition ease-out duration-300"
+                  enterFrom="transform scale-100 -translate-x-8"
+                  enterTo="transform scale-100 -translate-x-0"
+                  leave="transform duration-200 transition ease-in-out"
+                  leaveFrom="opacity-100 rotate-0 scale-100"
+                  leaveTo="opacity-0 scale-75 transform -translate-x-[4rem]"
+                >
+                  <Note
+                    item={item}
+                    refetch={items.refetch}
+                    setNotes={setNotes}
+                    notes={notes}
+                  />
+                </Transition.Child>
+              ))
+            : notes.map((item) => (
+                <Transition.Child
+                  key={item.id}
+                  appear={true}
+                  enter="transition ease-out duration-300"
+                  enterFrom="transform scale-100 -translate-x-8"
+                  enterTo="transform scale-100 -translate-x-0"
+                  leave="transform duration-200 transition ease-in-out"
+                  leaveFrom="opacity-100 rotate-0 scale-100"
+                  leaveTo="opacity-0 scale-75 transform -translate-x-[4rem]"
+                >
+                  <Note
+                    item={item}
+                    refetch={items.refetch}
+                    setNotes={setNotes}
+                    notes={notes}
+                  />
+                </Transition.Child>
+              ))}
         </Transition>
       </div>
     </>
